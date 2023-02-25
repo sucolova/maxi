628979da54ca48c08df36c2c7fb928c1a108dba7
Lukas Melcher <lukas.melcher@gmail.com>
2023-02-25 11:05:27 +0100

    greeting/ home page styling

--- src/components/greeting.js
+++ src/components/greeting.js
@@ -2,11 +2,24 @@ function Greeting() {
     return (
         <main>
             <div className="wrapper">
-                <p className="target">Maximilian </p>
-                <p className="target">Maximilian </p>
-                <p className="target">Maximilian </p>
-                <p className="target">Maximilian </p>
-                <p className="target">Maximilian </p>
+                <p className="target">Service </p>
+                <p className="target">Beratung </p>
+                <p className="target">Dienstleistung </p>
+                <p className="target">Mechanik </p>
+            </div>
+            <div className="first">
+                <h1>Bring it all together</h1>
+                <p>the fastest way to combine your favorite tools and apis to build the fastes sithes, stores and apps for the web</p>
+            </div>
+            <div className="second">
+                <h2>Diese Kunden waren begeistert</h2>
+                <ul>
+                    <li>Österreichische Bundesbahnen</li>
+                    <li>Kastner und öhler</li>
+                    <li>trafik austria</li>
+                    <li>bäckerei steger</li>
+                    <li>reifeisen waren austria</li>
+                </ul>
             </div>
         </main>
     );
